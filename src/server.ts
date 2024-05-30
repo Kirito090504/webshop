d60689f54463bbba47ab29151fa5f90edfad7580
Kirito090504 <tenchieadolfo05@gmail.com>
2024-05-30 19:55:38 +0800

    Add files

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Serve the landing page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
